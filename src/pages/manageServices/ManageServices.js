06612b9af2648c7882820489940ab378470aa72f
Rafiqul Islam Tusher <md.rafiqul26997@gmail.com>
2022-04-28 02:16:28 +0600

    added manage services page and load services from db and added delete btn

--- src/pages/manageServices/ManageServices.js
+++ src/pages/manageServices/ManageServices.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Button } from 'react-bootstrap';
 import useServices from '../customHooks/useServices';
 
 const ManageServices = () => {
     const [services, setServices] = useServices()
     const deleteService = (id) => {
-        console.log(id);
+        fetch(`http://localhost:5000/services/${id}`, {
+           method: 'DELETE'
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.deletedCount === 1) {
+                    console.log("data delted");
+                } else {
+                    console.log("No documents matched the query. Deleted 0 documents.");
+                }  
+        })
     }
     return (
         <div className='text-center mt-5'>
             <div>
             {
-                services.map(service => <h5>{service.name} <Button onClick={()=>deleteService(service._id)}>Delete</Button></h5>)
+                services.map(service => <h5 key={service._id}>{service.name} <Button onClick={()=>deleteService(service._id)}>Delete</Button></h5>)
             }
             </div>
         </div>
